8fd7f2f950fd0ded1b05c8ac8b6dfe6bee758534
Peter A. <ink.splatters@pm.me>
2024-12-31 17:39:19 +0100

    swift-package-revolutionary (swift-tools-4.8 fork)

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
   platforms: [.macOS(.v13)],
   dependencies: [
     // Only used on Linux & Windows
-    .package(url: "https://github.com/apple/swift-crypto.git", "2.0.0"..<"3.0.0")
+    .package(url: "https://github.com/apple/swift-crypto.git", "2.0.0"..<"3.0.0"),
+    .package(url: "https://github.com/ink-splatters/swift-testing-revolutionary", exact: "0.3.2+swift-tools-4.8")
   ],
   targets: [
     .executableTarget(
