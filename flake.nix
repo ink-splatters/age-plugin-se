12542a7eb330082a130a60dc3f579e443dcff8da
Peter A. <ink.splatters@pm.me>
2025-01-08 10:36:01 +0100

    * framework: reimplementation using flake-parts * formatter: nixfmt-rfc-style ->  alejandra * derivation: removed redundant attrs:     - `__contentAddressed`     - `outputHashType`

--- flake.nix
+++ flake.nix
@@ -1,58 +1,74 @@
 {
+  description = "Age plugin for Apple's Secure Enclave";
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-
-    systems.url = "github:nix-systems/default-darwin";
-    flake-utils.inputs.systems.follows = "systems";
-
-    git-hooks = {
+    git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "flake-compat";
       };
     };
-    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
   nixConfig = {
     extra-substituters = [
       "https://aarch64-darwin.cachix.org"
       "https://pre-commit-hooks.cachix.org"
-      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "aarch64-darwin.cachix.org-1:mEz8A1jcJveehs/ZbZUEjXZ65Aukk9bg2kmb0zL9XDA="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
-  outputs =
-    {
-      flake-utils,
-      nixpkgs,
-      git-hooks,
+  outputs = inputs @ {
+    git-hooks-nix,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} ({
+      withSystem,
+      flake-parts-lib,
       ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pre-commit = pkgs.callPackage ./nix/pre-commit.nix {
-          inherit git-hooks system;
-          src = ./.;
-        };
-      in
-      {
-        checks = { inherit (pre-commit) check; };
+    }: let
+      pre-commit = import ./nix/pre-commit.nix {};
+      dev-shell = import ./nix/dev-shell.nix {};
+      systems = import inputs.systems;
 
-        apps = { inherit (pre-commit) install-hooks; };
+      inherit (flake-parts-lib) importApply;
+      flakeModule = importApply ./nix/flake-module.nix {inherit withSystem;};
+    in {
+      imports = [
+        git-hooks-nix.flakeModule
+        pre-commit
+        dev-shell
+        flakeModule
+      ];
 
-        formatter = pkgs.nixfmt-rfc-style;
-        devShells.default = pkgs.callPackage ./nix/dev-shell.nix { };
-        packages = {
-          default = pkgs.callPackage ./nix/package { };
-        };
-      }
-    );
+      inherit systems;
+
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+        packages.default = config.packages.age-plugin-se;
+      };
+
+      flake = {
+        inherit flakeModule;
+      };
+    });
 }
