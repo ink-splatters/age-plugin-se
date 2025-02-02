226ad37ac72d17ab838c449cdf3816460f44706f
Peter A. <ink.splatters@pm.me>
2025-02-02 14:47:41 +0100

    * partitioned flake using `flake-parts` partitions functionality     * outputHash support for per-system age-plugin-se FOD packages     * moved swiftpm2nix to `package` dir     * pre-commit: misc change to (commented out) shfmt     * Removed input 'systems'

--- flake.nix
+++ flake.nix
@@ -2,69 +2,50 @@
   description = "Age plugin for Apple's Secure Enclave";
 
   inputs = {
-    git-hooks-nix = {
-      url = "github:cachix/git-hooks.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-compat.follows = "flake-compat";
-      };
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
   };
 
   nixConfig = {
     extra-substituters = [
       "https://aarch64-darwin.cachix.org"
-      "https://pre-commit-hooks.cachix.org"
     ];
     extra-trusted-public-keys = [
       "aarch64-darwin.cachix.org-1:mEz8A1jcJveehs/ZbZUEjXZ65Aukk9bg2kmb0zL9XDA="
-      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
     ];
   };
 
-  outputs = inputs @ {
-    git-hooks-nix,
-    flake-parts,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} ({
-      withSystem,
-      flake-parts-lib,
-      ...
-    }: let
-      pre-commit = import ./nix/pre-commit.nix {};
-      dev-shell = import ./nix/dev-shell.nix {};
-      systems = import inputs.systems;
-
-      inherit (flake-parts-lib) importApply;
-      flakeModule = importApply ./nix/flake-module.nix {inherit withSystem;};
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} (let
+      flakeModule = import ./nix/flake-module.nix {};
     in {
       imports = [
-        git-hooks-nix.flakeModule
-        pre-commit
-        dev-shell
+        flake-parts.flakeModules.partitions
         flakeModule
       ];
 
-      inherit systems;
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
 
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: {
-        formatter = pkgs.alejandra;
+      perSystem = {config, ...}: {
         packages.default = config.packages.age-plugin-se;
+      };
+
+      partitionedAttrs = {
+        apps = "dev";
+        checks = "dev";
+        devShells = "dev";
+        formatter = "dev";
+      };
+      partitions.dev = {
+        extraInputsFlake = ./nix/dev;
+        module = {
+          imports = [./nix/dev/flake-module.nix];
+        };
       };
 
       flake = {
