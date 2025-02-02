60cc26a93b068b0555a4d5bb406f68db4bb5aa98
Peter A. <ink.splatters@pm.me>
2025-02-02 14:47:41 +0100

    * partitioned flake using `flake-parts` partitions functionality     * outputHash support for per-system age-plugin-se FOD packages     * moved swiftpm2nix to `package` dir     * pre-commit: misc change to (commented out) shfmt     * Removed input 'systems'

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -1,33 +1,9 @@
-_: {config, ...}: {
+_: {
   perSystem = {
-    lib,
     pkgs,
+    system,
     ...
-  }: let
-    inherit (pkgs) swiftpm swiftpm2nix;
-    inherit (pkgs.swift) stdenv swift;
-  in {
-    config.packages.age-plugin-se = stdenv.mkDerivation rec {
-      pname = "age-plugin-se";
-      version = "0.1.4";
-      src = ../.;
-
-      nativeBuildInputs = [swift swiftpm];
-
-      configurePhase =
-        lib.optionalString (!stdenv.hostPlatform.isDarwin)
-        (swiftpm2nix.helpers ./swiftpm2nix).configure;
-
-      installPhase = ''
-        binPath="$(swiftpmBinPath)"
-        mkdir -p $out/bin
-        cp $binPath/${pname} $out/bin/
-      '';
-
-      enableParallelBuilding = true;
-
-      outputHashMode = "recursive";
-      outputHash = "sha256-OySBeUUk9ryYGEXvuBTC5G9ccsylLlq9M59+4DffHt8=";
-    };
+  }: {
+    config.packages.age-plugin-se = (pkgs.callPackage ./package {}).${system};
   };
 }
